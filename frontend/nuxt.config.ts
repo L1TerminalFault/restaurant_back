af967c0834331ece6b79756e6fe45e0fe3afb3a7
x <x>
2026-08-30 06:50:22 +0300

    updates4

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,6 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiBase: 'https://restaurant-backend-2d2w.vercel.app/api/v1'
+    }
+  },
   css: ['~/assets/css/main.css'],
     app: {
         head: {
@@ -12,7 +17,7 @@ export default defineNuxtConfig({
             { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }
           ],
           link: [
-            { rel: 'icon', type: 'image/png', href: '/assets/awaze_logo.png' },
+            { rel: 'icon', type: 'image/png', href: '/assets/images/awaze_logo.png' },
             { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
             { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
             {
